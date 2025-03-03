a6b75f0d0812ab5a8bcb0cf33de3250154074e7b
Steven Sarber <sesarber@gmail.com>
2025-03-02 20:51:26 -0800

    configuration success indication

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,6 +17,7 @@ export default function HomeScreen() {
   const [intervals, setIntervals] = useState(initialIntervals)
   const [showingIntervalFormView, setShowingIntervalFormView] = useState(false)
   const [bluetoothConnectionEstablished, setBluetoothConnectionEstablished] = useState(false)
+  const [configurationSuccess, setConfigurationSuccess] = useState(false)
 
   function handleIntervalSubmit(index: number, newTime: number, newDistance: number) {
     // If adding a new interval
@@ -78,56 +79,70 @@ export default function HomeScreen() {
             Workout Configuration
           </Text>
 
-          <ScrollView>
-            <View style={styles.intervalsContainer}>
-              <View style={styles.headingsContainer}>
-                <Text>Int</Text>
-                <Text>Distance (m)</Text>
-                <Text>Time (sec)</Text>
-              </View>
+          {!configurationSuccess &&
+            <ScrollView>
+              <View style={styles.intervalsContainer}>
+                <View style={styles.headingsContainer}>
+                  <Text>Int</Text>
+                  <Text>Distance (m)</Text>
+                  <Text>Time (sec)</Text>
+                </View>
 
-              {intervals.map((interval) => (
-                <IntervalView
-                key={interval.index} // necessary for React to manipulate the DOM
-                interval={interval}
-                onEditSubmit={(newTime: number, newDistance: number) => handleIntervalSubmit(interval.index, newTime, newDistance)}
-                onDelete={() => handleIntervalDeletion(interval.index)}
-                />
-              ))}
-
-
-              {showingIntervalFormView && // Conditionally render form
-                <IntervalFormView
-                index={intervals.length + 1}
-                defaultTime={NaN}
-                defaultDist={NaN}
-                onSubmit={(newTime: number, newDistance: number) => handleIntervalSubmit(intervals.length + 1, newTime, newDistance)}
-                onDelete={() => {
-                  setShowingIntervalFormView(false);
-                }}
-                />
-              }
-
-            
-              <Pressable
-                style={styles.addIntervalButton}
-                onPress={() => setShowingIntervalFormView(true)}
-                >
-                <Image
-                  style={{height: '100%', width: 30}}
-                  source={require('../../assets/images/plus-icon.png')}
-                  resizeMode='contain'
+                {intervals.map((interval) => (
+                  <IntervalView
+                  key={interval.index} // necessary for React to manipulate the DOM
+                  interval={interval}
+                  onEditSubmit={(newTime: number, newDistance: number) => handleIntervalSubmit(interval.index, newTime, newDistance)}
+                  onDelete={() => handleIntervalDeletion(interval.index)}
                   />
+                ))}
 
-                <Text style={{color: garminBlue}}>Add Interval</Text>
-              </Pressable>  
+
+                {showingIntervalFormView && // Conditionally render form
+                  <IntervalFormView
+                  index={intervals.length + 1}
+                  defaultTime={NaN}
+                  defaultDist={NaN}
+                  onSubmit={(newTime: number, newDistance: number) => handleIntervalSubmit(intervals.length + 1, newTime, newDistance)}
+                  onDelete={() => {
+                    setShowingIntervalFormView(false);
+                  }}
+                  />
+                }
+
+              
+                <Pressable
+                  style={styles.addIntervalButton}
+                  onPress={() => setShowingIntervalFormView(true)}
+                  >
+                  <Image
+                    style={{height: '100%', width: 30}}
+                    source={require('../../assets/images/plus-icon.png')}
+                    resizeMode='contain'
+                    />
+
+                  <Text style={{color: garminBlue}}>Add Interval</Text>
+                </Pressable>  
+              </View>
+            </ScrollView>
+          }
+
+          {configurationSuccess &&
+            <View style={styles.configSuccessContainer}>
+              <Image
+                source={require('../../assets/images/circle-checkmark-icon.png')}
+              />
+
+              <Text>Robot configured successfully!</Text>
             </View>
-          </ScrollView>
+          }
+          
         </View>
 
         <View style={styles.buttonsContainer}>
           <Pressable
             style={(bluetoothConnectionEstablished && intervals.length > 0) ? styles.button : styles.disabledButton}
+            disabled={configurationSuccess}
             onPress={() => {
               if (bluetoothConnectionEstablished && (intervals.length > 0)) {
                 Alert.alert(
@@ -141,7 +156,7 @@ export default function HomeScreen() {
                     {
                         text: "Configure",
                         onPress: () => {
-                          console.log("Configure button pressed")
+                          setConfigurationSuccess(true)
                         },
                         style: 'default'
                     }
@@ -163,6 +178,7 @@ export default function HomeScreen() {
 
           <Pressable
             style={intervals.length > 0 ? styles.button : styles.disabledButton}
+            disabled={configurationSuccess}
             onPress={() => {
               if (intervals.length > 0) {
                 Alert.alert(
@@ -224,6 +240,16 @@ const styles = StyleSheet.create({
   },
   workoutContainer: {
     flex: 1,
+    gap: 10,
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: 'white',
+  },
+  configSuccessContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
     gap: 10,
     padding: 20,
     borderRadius: 15,
